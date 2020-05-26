bc7f128f9bddd5c6562fd7c72e96c509d92ab4bc
HariPrasad005 <hari.8324@gmail.com>
2020-05-27 00:20:33 +0530

    Game(Mini Version)

---
+++ Scripts/AdventureGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State State;
+    void Start()
+    {
+        State = startingState;
+        textComponent.text = State.getStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        manageState();   
+    }
+
+    private void manageState()
+    {
+        var nextStates = State.getNextState();
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                State = nextStates[index];
+            }
+        }
+        textComponent.text = State.getStateStory();
+    }
+}
